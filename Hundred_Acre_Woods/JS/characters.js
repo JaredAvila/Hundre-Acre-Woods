1107a4344733693c983abaf7c550c6dd47c4edd2
Jared Avila <jared.c.avila@gmail.com>
2018-10-29 11:28:02 -0700

    updated objects and traversal

--- Hundred_Acre_Woods/JS/characters.js
+++ Hundred_Acre_Woods/JS/characters.js
@@ -1,35 +1,98 @@
-var winnie = {character : "Winnie the Pooh"}
-winnie.north = chrisR;
-winnie.south = tigger;
-winnie.west = piglet;
-winnie.east = bees;
-var chrisR = {character : "Christopher Robin"}
-chrisR.north = kanga;
-chrisR.south = winnie;
-chrisR.west = owl;
-chrisR.east = rabbit;
-var rabbit = {character : "Rabbit"}
-rabbit.south = bees;
-rabbit.west = chrisR;
-rabbit.east = gopher;
-var tigger = {character : "Tigger"}
-tigger.north = winnie;
-var piglet = {character : "Piglet"}
-piglet.north = owl;
-piglet.east = winnie;
-var owl = {character : "Owl"}
-owl.east = chrisR;
-owl.south = piglet;
-var bees = {character : "Bees"}
-bees.north = rabbit;
-bees.west = winnie;
-var gopher = {character : "Gopher"}
-gopher.west = rabbit;
-var kanga = {character : "Kanga"}
-kanga.north = eeyore;
-kanga.south = chrisR;
-var eeyore = {character : "Eeyore"}
-eeyore.south = kanga;
-eeyore.east = heffalumps;
-var heffalumps = {character : "Heffalumps"}
-heffalumps.west = eeyore;
+
+var winnie = {
+    character: "Winnie the Pooh"
+}
+
+var chrisR = {
+    character: "Christopher Robin"
+}
+var rabbit = {
+    character: "Rabbit"
+}
+var tigger = {
+    character: "Tigger",
+    north: winnie
+}
+var piglet = {
+    character: "Piglet",
+    east: winnie
+}
+var owl = {
+    character: "Owl",
+    east: chrisR,
+    south: piglet
+}
+var bees = {
+    character: "Bees",
+    north: rabbit,
+    west: winnie
+}
+var gopher = {
+    character: "Gopher",
+    west: rabbit
+}
+var kanga = {
+    character: "Kanga",
+    south: chrisR
+}
+var eeyore = {
+    character: "Eeyore",
+    south: kanga
+}
+var heffalumps = {
+    character: "Heffalumps",
+    west: eeyore
+}
+winnie.north = chrisR
+winnie.south = tigger
+winnie.west = piglet
+winnie.east = bees
+chrisR.north =  kanga
+chrisR.south =  winnie
+chrisR.west = owl
+chrisR.east = rabbit
+rabbit.south = bees
+rabbit.west = chrisR
+rabbit.east = gopher
+piglet.north = owl
+kanga.north = eeyore
+eeyore.east = heffalumps
+var player = {
+}
+
+player.location = eeyore;
+function move(dirr) {
+
+    if (dirr === "South") {
+        if (player.location.south != undefined) {
+            player.location = player.location.south;
+            alert("You have arrived at " + player.location.character + "'s house.");
+        } else {
+            alert("You can't go that way!");
+        }
+    }
+    if (dirr === "North") {
+        if (player.location.north != undefined) {
+            player.location = player.location.north;
+            alert("You have arrived at " + player.location.character + "'s house.");
+        } else {
+            alert("You can't go that way!");
+        }
+    }
+    if (dirr === "West") {
+        if (player.location.west != undefined) {
+            player.location = player.location.west;
+            alert("You have arrived at " + player.location.character + "'s house.");
+        } else {
+            alert("You can't go that way!");
+        }
+    }
+    if (dirr === "East") {
+        if (player.location.east != undefined) {
+            player.location = player.location.east;
+            alert("You have arrived at " + player.location.character + "'s house.");
+        } else {
+            alert("You can't go that way!");
+        }
+    }
+}
